5c08777c8ea18e52dc23221eb9ecbdf375e13192
1natsu <1natsu172@users.noreply.github.com>
2020-10-29 01:40:00 +0900

    chore: separate jest config

---
+++ bluevery/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  modulePathIgnorePatterns: ['<rootDir>/__tests__/__utils__'],
+};
