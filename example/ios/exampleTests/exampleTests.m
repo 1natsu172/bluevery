7d63a0743ccafad2119209bf30fbf6508c83f3df
1natsu <1natsu172@users.noreply.github.com>
2020-11-10 21:53:03 +0900

    fix: break & re-create example app

--- example/ios/exampleTests/exampleTests.m
+++ example/ios/exampleTests/exampleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface blueveryTests : XCTestCase
+@interface exampleTests : XCTestCase
 
 @end
 
-@implementation blueveryTests
+@implementation exampleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
--- example/metro.config.js
+++ example/metro.config.js
@@ -4,8 +4,17 @@
  *
  * @format
  */
+const path = require('path');
+const blacklist = require('metro-config/src/defaults/blacklist');
+const escape = require('escape-string-regexp');
+const parentPak = require('../package.json');
+
+const rootPath = path.resolve(__dirname, '..');
+const modules = ['@babel/runtime', ...Object.keys(parentPak.peerDependencies)];
 
 module.exports = {
+  projectRoot: __dirname,
+  watchFolders: [rootPath],
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -13,5 +22,20 @@ module.exports = {
         inlineRequires: false,
       },
     }),
+  },
+  resolver: {
+    blacklistRE: blacklist(
+      modules.map(
+        (module) =>
+          new RegExp(
+            `^${escape(path.join(rootPath, 'node_modules', module))}\\/.*$`,
+          ),
+      ),
+    ),
+
+    extraNodeModules: modules.reduce((acc, module) => {
+      acc[module] = path.join(__dirname, 'node_modules', module);
+      return acc;
+    }, {}),
   },
 };
