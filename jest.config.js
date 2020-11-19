bb5f040deb8dd50409685347fb6ed167e064462e
1natsu <1natsu172@users.noreply.github.com>
2020-11-20 02:20:27 +0900

    feat: coverage action

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFiles: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/example'],
   modulePathIgnorePatterns: ['<rootDir>/__tests__/__utils__'],
 };
